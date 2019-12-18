022c5e030a25bade29676fd41d249184733c1334
atleastzero <megan.obryan52@gmail.com>
2019-12-18 13:43:23 -0800

    Scrapped characters for universes.

--- characters/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.CharactersSavedListView.as_view(), name='characters-saved-list'),
-]
--- characters/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-
-class CharactersSavedListView(ListView):
-    model = Character
-
-    def get(self, request):
-        characters = self.get_queryset().all()
-        return render(request, 'characters/character_saved_list.html')
