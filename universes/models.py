1d1081a2aa976c23f8ed4d1b86448f502b062e35
atleastzero <megan.obryan52@gmail.com>
2019-12-18 22:06:13 -0800

    Added place model.

--- universes/models.py
+++ universes/models.py
@@ -8,11 +8,22 @@ UNIVERSE_GENRE = (
 )
 
 class Universe(models.Model):
-    name = models.CharField(max_length=50)
-    genre = models.CharField(max_length=20, choices=UNIVERSE_GENRE, default='fa')
-    universe_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    last_updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+    name                = models.CharField(max_length=50)
+    genre               = models.CharField(max_length=20, choices=UNIVERSE_GENRE, default='fa')
+    universe_id         = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    last_updated        = models.DateTimeField(auto_now=True)
+    created             = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class Place(models.Model):
+    name                = models.CharField(max_length=50)
+    size                = models.CharField(max_length=200)
+    total_population    = models.IntegerField()
+    place_id            = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class PlaceDemographics(models.Model):
+    place               = models.ForeignKey(Place, on_delete=models.CASCADE)
+  # race                = models.ForeignKey(Character, on_delete=models.CASCADE)
+    population          = models.IntegerField()
