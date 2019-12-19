43fa5aeaee222129278c9a2109a2c25ba50e31bc
atleastzero <megan.obryan52@gmail.com>
2019-12-18 21:19:45 -0800

    Added universe detail view.

--- universes/views.py
+++ universes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 
 from universes.models import Universe
 from universes.forms import UniverseCreateForm
@@ -12,6 +13,15 @@ class UniverseListView(ListView):
         universes = self.get_queryset().all().order_by("-last_updated")
         return render(request, 'universes/universe_list.html', {
             'universe': universes
+        })
+
+class UniverseDetailView(DetailView):
+    model = Universe
+
+    def get(self, request, universe_id):
+        universe = get_object_or_404(Universe, universe_id__exact=universe_id)
+        return render(request, 'universes/universe_detail.html', {
+            'universe': universe
         })
 
 class UniverseCreateView(CreateView):
